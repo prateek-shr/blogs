19553e86ba20f9c9c68d97425bb8a189ce7e373a
Prateek Sharma <prateekshr2@gmail.com>
2022-04-19 11:59:56 +0530

    Update mongo.js

--- configerations/mongo.js
+++ configerations/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
  
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://blog:MNNfPG3n2oKpTt1Z@cluster0-shard-00-00.06vc6.mongodb.net:27017,cluster0-shard-00-01.06vc6.mongodb.net:27017,cluster0-shard-00-02.06vc6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-t3vou6-shard-0&authSource=admin&retryWrites=true&w=majority", {
+        const conn = await mongoose.connect("mongodb+srv://prateek:123@cluster0.z2ilp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
         });
